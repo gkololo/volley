c14dcc7033290fcf30e16af37c77227fe1a43701
gkololo <gkoweb974@gmail.com>
2025-07-28 16:34:17 +0400

    nettoyage des copies

--- saisie_equipes/admin copy.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Declaration, CategorieAge, Sexe, Zone, Club
-
-# Register your models here.
-@admin.register(Declaration)
-class DeclarationAdmin(admin.ModelAdmin):
-    list_display = ("club", "declarant", "nombre_equipes", "categorie_age", "sexe", "zone", "date_tournoi")
-
-admin.site.register(Club)
